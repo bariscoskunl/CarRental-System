d6ff7aec22ea321b72722c16744b54df7bc372e2
GorkemKutbay <kutbaygorkem2007@gmail.com>
2026-06-08 23:18:10 +0300

    some services and repo's added

---
+++ CarRentalSystem.Business/Services/Abstract/ICommentService.cs
@@ -0,0 +1,29 @@
+﻿using CarRentalSystem.Business.DTOs.Comment;
+using CarRentalSystem.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.Business.Services.Abstract
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<CommentDto>> GetCommentsByCarIdAsync(int carId);
+        Task<IEnumerable<CommentDto>> GetCommentsByCustomerIdAsync(int customerId);
+        Task<IEnumerable<CommentDto>> GetCommentsByCompanyIdAsync(int companyId);
+        Task AddReply(CreateCommentReplyDto comment, int parentId);
+        Task<IEnumerable<CommentReplyDto>> GetRepliesByCustomerIdAsync(int customerId, int parentCommentId);
+        Task<IEnumerable<CommentReplyDto>> GetReplies(int parentCommentId);
+        Task<IEnumerable<CommentDto>> GetWithReplies(int parentCommentId);
+        Task<IEnumerable<CommentDto>> GetAll();
+        Task CreateToCompany(CommentCreateDto dto, int companyId);
+        Task CreateToCar(CommentCreateDto dto, int carId);
+        Task Update(CommentDto comment);
+        Task DeleteComment(int commentId);
+
+
+
+    }
+}
---
+++ CarRentalSystem.DataAccess/Interfaces/ICommentRepository.cs
@@ -0,0 +1,22 @@
+﻿using CarRentalSystem.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.DataAccess.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetCommentsByCarIdAsync(int carId);
+        Task<IEnumerable<Comment>> GetCommentsByCustomerIdAsync(int customerId);
+        Task<IEnumerable<Comment>> GetCommentsByCompanyIdAsync(int companyId);
+        Task AddReply(Comment comment, int parentId);
+        Task<IEnumerable<Comment>> GetRepliesByCustomerIdAsync(int customerId, int parentCommentId);
+        Task<IEnumerable<Comment>> GetReplies(int parentCommentId);
+        Task<IEnumerable<Comment>> GetWithReplies(int parentCommentId);
+
+
+    }
+}
