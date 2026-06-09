c4ae52fd5b01afff9a1be839653de6f9c55b1546
BARIŞ COŞKUN <bariscoskun441@gmail.com>
2026-06-09 18:03:14 +0300

    feat(business): implement company service and repository interfaces

---
+++ CarRentalSystem.Business/Services/Abstract/ICompanyService.cs
@@ -0,0 +1,24 @@
+﻿using CarRentalSystem.Business.DTOs.Company;
+using CarRentalSystem.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.Business.Services.Abstract
+{
+    public interface ICompanyService 
+    {
+
+        Task<CompanyCreateDto> CreateAsync(CompanyCreateDto companyCreateDto);
+        Task<IEnumerable<CompanyDto>> GetAllAsync (int? pageSize = null);
+        Task<CompanyDto> UpdateAsync (CompanyDto companyDto);
+        Task<bool> DeleteAsync(int CompanyId);
+        Task<IEnumerable<CompanyDto>> GetCompaniesByCityAsync(string city);
+        Task<IEnumerable<CompanyDto>> GetCompaniesByCountryAsync(string country);
+        Task<IEnumerable<CompanyDto>> GetCompaniesByDistrictAsync(string district);
+
+
+    }
+}
