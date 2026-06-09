9c15cb195afeceb687da5d0f5ce27d63cf6882a6
BARIŞ COŞKUN <bariscoskun441@gmail.com>
2026-06-09 18:04:32 +0300

    feat(car-image): implement car image infrastructure with business rules

---
+++ CarRentalSystem.DataAccess/Interfaces/ICarImageRepository.cs
@@ -0,0 +1,15 @@
+﻿using CarRentalSystem.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.DataAccess.Interfaces
+{
+    public interface ICarImageRepository : IRepository<CarImage>
+    {
+        Task<IEnumerable<CarImage>> GetImagesByCarIdAsync(int carId);
+        Task<CarImage?> GetCoverImageByCarIdAsync(int carId);
+    }
+}
